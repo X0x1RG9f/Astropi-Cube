c52a220e05130446ac98fd427e246ca50d873264
Ludovic Courgnaud <ludovic.courgnaud@gmail.com>
2023-09-09 14:38:29 +0200

    Update install.sh

--- AstroPi Cube/Install/install.sh
+++ AstroPi Cube/Install/install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-[ "$PWD" != "/opt/astropi" ] || { echo "Script not executed from /opt/astropi/. Exiting." >&2; exit 1; }
 
+####################################
+#           DEPENDENCIES           #
+####################################
 sudo apt-get update -y
 sudo apt-get full-upgrade -y
 sudo apt-get install --purge virtualgps
-
 sudo apt-get install git php-fpm mariadb-server php-mysql -y
 pip3 install mysql-connector
 pip install mysql-connector
@@ -14,35 +15,53 @@ pip install psutil
 pip3 install adafruit-mcp3008
 pip install adafruit-mcp3008
 
-cp ./config/astroberry /etc/nginx/sites-enabled/astroberry
 
-sudo service php*-fpm restart
-sudo service nginx restart
-
-# cp * /opt/astropi ?
-# chmod -R 755 /opt/astropi
-
-echo "www-data ALL=(ALL) NOPASSWD:/sbin/shutdown" >> /etc/sudoers
-echo "www-data ALL=(ALL) NOPASSWD:/sbin/reboot" >> /etc/sudoers
-
-git clone https://github.com/X0x1RG9f/AdminLTE.git /var/www/html/astropi/
-git clone https://github.com/X0x1RG9f/BitBangingDS18B20.git /opt/astropi/DS18B20/
-
-chmod -Rf 755 /var/www/html/astropi/*
-chmod -Rf 755 /opt/astropi/*
+####################################
+#           COPY USEFUL            #
+####################################
+sudo mkdir /opt/astropi-cube/
+sudo cp -Rf ./Astropi-Cube/AstroPi\ Cube/Install/scripts/* /opt/astropi-cube/
+sudo cp -Rf ./Astropi-Cube/AstroPi\ Cube/Install/config/* /opt/astropi-cube/
+sudo cp -f ./Astropi-Cube/AstroPi\ Cube/Install/config/astroberry /etc/nginx/sites-enabled/astroberry
+sudo rm -Rf /var/www/html
+sudo mkdir /var/www/html/
+sudo mkdir /var/www/html/astropi-cube/
+sudo cp -Rf ./Astropi-Cube/AstroPi\ Cube/Install/www/* /var/www/html/astropi-cube/
+sudo chmod -Rf 755 /var/www/html/astropi-cube/*
+sudo chmod -Rf 755 /opt/astropi-cube/*
 
 
-line="@reboot sleep 5; /usr/bin/python /opt/astropi/scripts/monitoring/raspberry.py >> /tmp/astropi.log 2>&1"
-(crontab -l; echo "$line" ) | crontab -
-line="@reboot sleep 7; /usr/bin/python /opt/astropi/scripts/monitoring/modes.py >> /tmp/astropi.log 2>&1"
-(crontab -l; echo "$line" ) | crontab -
-line="@reboot sleep 9; /usr/bin/python /opt/astropi/scripts/monitoring/power_probes.py >> /tmp/astropi.log 2>&1"
-(crontab -l; echo "$line" ) | crontab -
-line="@reboot sleep 11; /usr/bin/python /opt/astropi/scripts/monitoring/dew_probes.py >> /tmp/astropi.log 2>&1"
-(crontab -l; echo "$line" ) | crontab -
-line="@reboot sleep 11; /usr/bin/python /opt/astropi/scripts/monitoring/weather_probes.py >> /tmp/astropi.log 2>&1"
-(crontab -l; echo "$line" ) | crontab -
+####################################
+#       REMOVE NOT NECESSARY       #
+####################################
+sudo rm -Rf ./Astropi-Cube
+sudo rm -f ./Astropi-Cube/AstroPi\ Cube/Install/config/astroberry
 
-sed -i -e "s/dtoverlay/#dtoverlay/g" /boot/config.txt
+
+####################################
+#      DEPENDENCIES AGAIN          #
+####################################
+git clone https://github.com/X0x1RG9f/AdminLTE.git /var/www/html/astropi-cube/
+git clone https://github.com/X0x1RG9f/BitBangingDS18B20.git /opt/astropi-cube/DS18B20/
+
+
+####################################
+#           CONFIGURATION          #
+####################################
+sudo echo "www-data ALL=(ALL) NOPASSWD:/sbin/shutdown" >> /etc/sudoers
+sudo echo "www-data ALL=(ALL) NOPASSWD:/sbin/reboot" >> /etc/sudoers
+sudo sed -i -e "s/dtoverlay/#dtoverlay/g" /boot/config.txt
+sudo /usr/bin/mysql -uroot < /opt/astropi-cube/config/mysql.file
+
+####################################
+#          STARTUP CONFIG          #
+####################################
+sudo cp ./Astropi-Cube/AstroPi\ Cube/Install/scripts/startup/astropicube.service /etc/systemd/system/
+/bin/systemctl enable astropicube.service
+
+####################################
+#         RESTART & DONE!          #
+####################################
+#sudo service php*-fpm restart
+#sudo service nginx restart
 reboot
-
